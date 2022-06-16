3736561e2d7227f0aa09765df227e6ad2cb794d4
David Torres <dt@thehotelsnetwork.com>
2022-06-16 13:32:33 +0200

    fix: point to ip instead of localhost

--- cmd/common/flags/flags.go
+++ cmd/common/flags/flags.go
@@ -2,7 +2,7 @@ package flags
 
 import "flag"
 
-var Broker = flag.String("broker", "localhost:9092", "kafka broker to consume messages")
+var Broker = flag.String("broker", "127.0.0.1:9092", "kafka broker to consume messages")
 var Topic = flag.String("topic", "currencyUpdate", "kafka topic to consume messages")
 var Debug = flag.Bool("debug", false, "debug mode")
 var Trace = flag.Bool("trace", false, "trace mode")
